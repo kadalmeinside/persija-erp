93dec2746bce706717c9e7ca588f49ed5ded6d43
MacBook <macbook@MacBooks-MacBook-Pro.local>
2026-09-21 16:33:48 +0700

    feat: add loading animation and prevent double clicks on employee form submit

--- resources/js/Components/EmployeeFormModal.vue
+++ resources/js/Components/EmployeeFormModal.vue
@@ -413,8 +413,12 @@ const close = () => {
 
             <div class="mt-6 flex justify-end">
                 <SecondaryButton @click="close"> Batal </SecondaryButton>
-                <PrimaryButton class="ml-3" :class="{ 'opacity-25': form.processing }" :disabled="form.processing" @click="submit">
-                    Simpan
+                <PrimaryButton class="ml-3" :class="{ 'opacity-25 cursor-not-allowed': form.processing }" :disabled="form.processing" @click="submit">
+                    <svg v-if="form.processing" class="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                    {{ form.processing ? 'Menyimpan...' : 'Simpan' }}
                 </PrimaryButton>
             </div>
         </div>
